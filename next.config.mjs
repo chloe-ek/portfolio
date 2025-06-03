c52c3c29ad98ff8b1fdaeff466bf6369ae8b0aaa
chloe-ek <chloe.kwon08@gmail.com>
2025-06-02 18:21:03 -0700

    Add deploy script for GitHub pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? '/Portfolio' : '', 
+};
 
 export default nextConfig;
