99fd1de2a15871f9d43462a92af0b16dfcb7f6f2
chloe-ek <chloe.kwon08@gmail.com>
2025-06-02 18:57:08 -0700

    Fix gitignore file to include out folder

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,12 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   output: 'export',
+  reactStrictMode: true,
   images: {
     unoptimized: true,
   },
   basePath: isProd ? '/Portfolio' : '', 
+  assetPrefix: isProd ? '/Portfolio/' : '',
 };
 
 export default nextConfig;
